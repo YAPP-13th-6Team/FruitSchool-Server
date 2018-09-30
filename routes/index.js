091edc14e2327fe24f3905660d0c0a6fba91c95e
ekdud2412 <dayoung9650@gmail.com>
2018-09-30 18:12:27 +0900

    [수정] routes 구조 변경2

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 const fruit = require('./fruit');
 const common_sense = require('./common_sense');
+const auth = require('./auth')
 
 router.use("/fruits", fruit);
 router.use("/commonsenses", common_sense);
+router.use("/auth", auth);
 
 module.exports = router
