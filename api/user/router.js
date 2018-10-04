cf99535eed3e60cc50b3cd24bd5015df6954a659
presto <yoohan95@gmail.com>
2018-10-04 10:56:36 +0900

    [삭제] /api/user

--- api/user/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("./router.controller")
-
-router.post("/user", controller.createUser)
-router.post("/duplicated", controller.checkDuplicatedUser)
-router.post("/grade", controller.updateGrade)
-
-module.exports = router
