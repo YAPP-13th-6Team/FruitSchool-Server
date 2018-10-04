9158d21e97b1407a24164c52974d1f24ebbd693d
Seungyeon Lee <adie0423@gmail.com>
2018-10-05 01:37:33 +0900

    [추가] 커뮤니티 라우터 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ var router = express.Router()
 const fruit = require('./fruit')
 const common_sense = require('./common_sense')
 const user = require('./user')
+const community = require('./community')
 
 router.use("/fruits", fruit)
 router.use("/commonsenses", common_sense)
 router.use("/users", user)
+router.use("posts", community)
 
 module.exports = router
