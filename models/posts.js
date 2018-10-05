aa4da634346f1b631bb8f097bfa4f8410964af6f
Seungyeon Lee <adie0423@gmail.com>
2018-10-05 22:09:47 +0900

    [수정] 커뮤니티 post 스키마 수정

--- models/posts.js
+++ models/posts.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose"), Schema = mongoose.Schema;
+const moment = require("moment");
 
 const Post = new mongoose.Schema({
-    author: {type: Schema.Types.ObjectId, ref: 'user', required: true},
-    content: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'User'},
+    content: { type: String },
     createdAt: {type: Date, default: Date.now},
-    heart: [{type: Schema.Types.ObjectId, ref: 'user'}],
+    likes: {type: Number, default: 0},
+    heart: [{type: Schema.Types.ObjectId, ref: 'User'}],
     tag:[{type:String, required: false}],
     post_image: [{type: String, required: false}],
     comments: [{
-        author: {type: Schema.Types.ObjectId, ref: 'user', required: true},
+        author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
         content: {type: String, required: true},
         createdAt: {type: Date, default: Date.now},
     }]
