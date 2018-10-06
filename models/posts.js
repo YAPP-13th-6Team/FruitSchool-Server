4d7f44f41986712cd983d10e1fcaeb2e1613f067
이승연 <adie0423@gmail.com>
2018-10-06 15:38:06 +0900

    [수정] 날짜 부분 moment로 변경

--- models/posts.js
+++ models/posts.js
@@ -4,7 +4,7 @@ const moment = require("moment");
 const Post = new mongoose.Schema({
     author: {type: Schema.Types.ObjectId, ref: 'User'},
     content: { type: String },
-    createdAt: {type: Date, default: Date.now},
+    createdAt: {type: Date, default: moment().format("YYYY-MM-DD HH:mm:ss")},
     likes: {type: Number, default: 0},
     heart: [{type: Schema.Types.ObjectId, ref: 'User'}],
     tag:[{type:String, required: false}],
@@ -13,7 +13,7 @@ const Post = new mongoose.Schema({
     comments: [{
         author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
         content: {type: String, required: true},
-        createdAt: {type: Date, default: Date.now},
+        createdAt: {type: Date, default: moment().format("YYYY-MM-DD HH:mm:ss")},
     }]
 }, {
     versionKey: false
